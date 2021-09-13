a1ed8098f7200382cb26edeb07c34fe1de0e012a
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2021-09-13 20:02:29 -0300

    Withdrawal operation working

--- src/index.js
+++ src/index.js
@@ -22,6 +22,17 @@ function verifyIfExistsAccountCPF(req, res, next){
   return next();
 }
 
+function getBalance(statement){
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === "credit"){
+      return acc + operation.amount;
+    }
+    return acc - operation.amount;
+  }, 0);
+
+  return balance
+}
+
 app.post("/account", (req, res)=>{
   const { cpf, name } = req.body;
 
@@ -62,6 +73,26 @@ app.post("/deposit", verifyIfExistsAccountCPF, (req, res)=>{
     created_at: new Date(),
     type: "credit"
   }
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res)=>{
+  const { amount } = req.body;
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+  if(balance<amount){
+    return res.status(400).json({error: "Insufficient funds!"});
+  };
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  };
 
   customer.statement.push(statementOperation);
 
