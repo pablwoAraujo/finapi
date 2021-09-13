9330d1ff439a97fc2be3a64715ae175cef22e490
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2021-09-13 17:32:35 -0300

    Account creation working

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+
+app.post("/account", (req, res)=>{
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+  
+  if(customerAlreadyExists){
+    return res.status(400).json({error: "Customer already Exists!"});
+  }
+
+  if(!cpf || !name){
+    return res.status(400).json({error: "Bad Request!"});
+  }
+ 
+  customers.push({
+    cpf, 
+    name, 
+    id: uuidv4(), 
+    statement: []
+  });
+
+  return res.status(201).send();
+});
+
+app.listen(3333);
